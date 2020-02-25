b7db24efd104e521846a0d13c49d9bcf9c6f8e36
Alikhan <autocardag@gmail.com>
2020-02-25 13:37:27 +0300

    Init on Github

---
+++ src/main/java/ru/gamania/logger/ModLogger.java
@@ -0,0 +1,188 @@
+package ru.gamania.logger;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartedEvent;
+import cpw.mods.fml.common.event.FMLServerStoppingEvent;
+
+import ru.gamania.logger.config.Config;
+import ru.gamania.logger.streams.FileReaderStream;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.logging.log4j.Logger;
+
+@Mod(
+    modid = "ModLogger",
+    name = "ModLogger",
+    version="1.0",
+    acceptableRemoteVersions = "*"
+)
+public class ModLogger
+{
+    @Instance
+    public static ModLogger instance;
+
+    public Logger logger = null;
+
+    private Config config;
+    
+    private final Timer timer1 = new Timer();
+    private final Timer timer2 = new Timer();
+
+    private FileReaderStream fileReaderStream;
+
+    private int lastSize;
+
+    // Database connection
+    private Connection conn = null;
+
+    // Message buffer used for log4j-based logging TODO uncomment
+    // private List<String> msgBuffer = new ArrayList<String>();
+
+    @Mod.EventHandler
+    public void onPreInit(final FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+        config = new Config(event.getSuggestedConfigurationFile());
+        fileReaderStream = new FileReaderStream(event.getModConfigurationDirectory());
+    }
+
+    @Mod.EventHandler
+    public void onServerStarted(final FMLServerStartedEvent event)
+    {
+        config.update();
+
+        // Configurator.initialize("MyName", "file:/D:/source/minecraft/mods/SocketLogger/src/main/resources/log4j.xml"); TODO fix
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            logger.fatal("Cannot find jdbc driver", e);
+        }
+
+        timer1.schedule(new TimerTask() {
+            boolean stackTracePrinted = false;
+
+            @Override
+            public synchronized void run() {
+                logger.info("[ModLogger] Trying to connect to database...");
+                try {
+                    conn = DriverManager.getConnection(
+                        "jdbc:mysql://" + config.getDbHost() + "/" + config.getDbName() + "?useUnicode=true&serverTimezone=UTC",
+                        config.getDbUser(),
+                        config.getDbPassword()
+                    );
+                    logger.info("[ModLogger] Connected to database successfully");
+                    timer1.cancel();
+                    timer1.purge();
+                    startLogging();
+                } catch (SQLException e) {
+                    final String msg = "[ModLogger] Unable to connect to database, retrying in " + config.getConnectRetryRate() + "ms";
+                    // Print stacktrace only for the first time of being unable to connect
+                    if (stackTracePrinted) {
+                        logger.error(msg);
+                    } else {
+                        logger.error(msg, e);
+                        stackTracePrinted = true;
+                    }
+                }
+            }
+        }, 0, config.getConnectRetryRate());
+    }
+
+    private void startLogging()
+    {
+        logger.info("[ModLogger] Started logging");
+        timer2.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public synchronized void run() {
+                List<String> lines = fileReaderStream.getLines();
+
+                if (lastSize != lines.size()) {
+                    StringBuilder sql = new StringBuilder("INSERT INTO `logs` (`server_uid`, `line`) VALUES");
+                    List<String> linesToSend = new ArrayList<String>();
+
+                    ListIterator<String> it = lines.listIterator(lastSize);
+                    lastSize = lines.size();
+
+                    while (it.hasNext()) {
+                        String line = it.next();
+                        if (isBlacklisted(line)) {
+                            continue;
+                        }
+
+                        sql.append(" (?, ?),");
+                        linesToSend.add(line);
+                    }
+
+                    if (0 == linesToSend.size()) {
+                        return;
+                    }
+
+                    try (PreparedStatement stmt = conn.prepareStatement(sql.substring(0, sql.length() - 1))){
+                        int paramIndex = 1;
+                        for (String line : linesToSend) {
+                            stmt.setInt(paramIndex++, config.getServerUid());
+                            stmt.setString(paramIndex++, line);
+                        }
+
+                        stmt.executeUpdate();
+                    } catch (SQLException e) {
+                        logger.error("[ModLogger] Unable to send logs", e);
+                    }
+                }
+            }
+        }, 0, config.getUpdateRate());
+    }
+
+    private boolean isBlacklisted(final String line)
+    {
+        for (String regex : config.getLogBlacklist()){
+            if (regex.startsWith("^")) {
+                regex = "^(?:\\[\\d{2}:\\d{2}:\\d{2}\\] )?" + regex.substring(1);
+            }
+            Pattern ptr = Pattern.compile(regex);
+            Matcher matcher = ptr.matcher(line);
+            if (matcher.find()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /* TODO uncomment and use for log4j-based logging
+    public static void log(String message) {
+        ModLogger.instance.msgBuffer.add(message);
+    }
+    */
+
+    @Mod.EventHandler
+    public void onServerStopping(final FMLServerStoppingEvent event)
+    {
+        if (null != conn) {
+            logger.info("[ModLogger] Closing database connection...");
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                logger.error("[ModLogger] An error occurred while attemptint to close the database connection", e);
+            }
+        }
+        timer1.cancel();
+        timer1.purge();
+        timer2.cancel();
+        timer2.purge();
+    }
+}
---
+++ src/main/java/ru/gamania/logger/config/Config.java
@@ -0,0 +1,109 @@
+package ru.gamania.logger.config;
+
+import net.minecraftforge.common.config.Configuration;
+
+import java.io.File;
+
+public class Config
+{
+    private File configFile;
+
+    private int serverUid;
+    private int updateRate;
+    private int connectRetryRate;
+
+    private String dbHost;
+    private String dbName;
+    private String dbUser;
+    private String dbPass;
+
+    private String[] logBlacklist;
+
+    public Config(final File file)
+    {
+        configFile = file;
+        this.update();
+    }
+
+    public void update()
+    {
+        Configuration cfg = new Configuration(configFile);
+
+        serverUid = cfg.getInt("serverUID", "general", 0, 0, Integer.MAX_VALUE,  "Server id (check https://console.gamania.ru/servers/ for id list)");
+        updateRate = cfg.getInt("updateRate", "general", 250, 50, Integer.MAX_VALUE, "How often are the logs being sent (in milliseconds)");
+
+        dbHost = cfg.getString("host", "database", "127.0.0.1", "IP address");
+        dbName = cfg.getString("name", "database", "socket_logger", "Database name");
+        dbUser = cfg.getString("user", "database", "root", "User");
+        dbPass = cfg.getString("password", "database", "", "Password");
+        connectRetryRate = cfg.getInt("retryRate", "database", 30000, 50, Integer.MAX_VALUE, "How often does the mod try to reconnect to the database (in milliseconds)");
+
+        logBlacklist = cfg.getStringList("blacklist", "logs", new String[] {
+            // Errors
+            "^\\[[^/]+/(?:WARN|ERROR)\\]",
+            "^\\[?(?:com|java|org|net|me)\\.",
+            "^\\tat",
+
+            // Commands
+            "^\\[Server thread/INFO\\]: \\w+ issued server command: /(?:/wand|we cui|rtp)",
+
+            // Server thread/INFO
+            "^\\[Server thread/INFO\\]: Injected",
+            "^\\[Server thread/INFO\\]: Sending server configs",
+            "^\\[Server thread/INFO\\]: Found mod",
+            "^\\[Server thread/INFO\\]: \\w+\\[.*?\\] logged in with entity",
+            "^\\[Server thread/INFO\\]: \\[Server thread\\] Server side modded connection established",
+            "^\\[Server thread/INFO\\]: \\[ONTIME\\]",
+            "^\\[Server thread/INFO\\]: Loading dimension",
+
+            // Other
+            "^\\[pool-\\d+-thread-\\d+/INFO\\]",
+            "^\\[main/INFO\\]: (?:Calling|Loading) tweak class",
+            "^\\[Netty IO #\\d+/INFO\\]: (?!\\[L|\\x1b\\[0;33;22mG\\] )",
+            "^\\[User Authenticator #\\d+/INFO\\]",
+
+        }, "Logs that are ignored and are not sent to the database");
+
+        cfg.save();
+    }
+
+    public int getServerUid()
+    {
+        return serverUid;
+    }
+
+    public int getUpdateRate()
+    {
+        return updateRate;
+    }
+
+    public int getConnectRetryRate()
+    {
+        return connectRetryRate;
+    }
+
+    public String getDbHost()
+    {
+        return dbHost;
+    }
+
+    public String getDbName()
+    {
+        return dbName;
+    }
+
+    public String getDbUser()
+    {
+        return dbUser;
+    }
+
+    public String getDbPassword()
+    {
+        return dbPass;
+    }
+
+    public String[] getLogBlacklist()
+    {
+        return logBlacklist;
+    }
+}
